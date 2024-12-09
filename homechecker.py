e359f8cbb6cf3566475050a7b25305930ce2b751
İbrahim Algaç <ialgac@42Istanbul-iMac.local>
2024-12-09 12:02:47 +0300

    logging

--- homechecker.py
+++ homechecker.py
@@ -2,6 +2,12 @@ import requests
 import ftlib
 import config
 import time
+import logging
+
+file_handler = logging.handlers.RotatingFileHandler("homechecker.log", maxBytes=5000, backupCount=5)
+file_handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
+logger = logging.getLogger("Homechecker")
+logger.addHandler(file_handler)
 
 api = ftlib.Ftlib(config.INTRA_APP_PUBLIC, config.INTRA_APP_SECRET)
 hed = {
@@ -15,6 +21,7 @@ def list_homes() -> list:
     if (homes.status_code == 200):
         return homes.json()
     else:
+        logger.error(f"Error homemaker api! - resp.status_code = {homes.status_code}")
         raise ConnectionError("status code is 200")
 
 def get_active_sessions() -> list:
@@ -31,12 +38,15 @@ def close(iqn : str, login : str):
     "login": login}
     resp = requests.put(f"{config.HOMEMAKER_ENDPOINT}/homes/umount", headers=hed, json=data)
     if (resp.status_code == 409):
+        logger.error("Runtime error! intra_app resp.status_code = 409")
         raise RuntimeError(f"No home with this name exposed! {iqn}, {login}")
     if (resp.status_code == 400):
+        logger.error("Runtime error! intra_app resp.status_code = 400")
         raise RuntimeError("error")
     if (resp.status_code == 404):
+        logger.error("Runtime error! intra_app resp.status_code = 404")
         raise RuntimeWarning(f"couldnt close for iqn : {iqn}, login : {login}")
-    print(iqn, login, " is closed")
+    logger.info(f"Iqn - > {iqn} , {login} is closed")
 
 def check_fails():
     actives = get_active_sessions()
@@ -44,15 +54,14 @@ def check_fails():
         user = i["identifier"]
         user_intra = api.Users.get_user_by_login(user)
         if (user_intra.location is None):
-            #close home
             close(i["exposed_to"], user)
-            ##
-            print("closed , ", user)
-            pass
-        print("not none, ", user)
-        time.sleep(0.5)
+        time.sleep(1)
 
 def main():
     while True:
+        logger.debug("Checking...")
         check_fails()
         time.sleep(config.PERCHECK_TIME_IN_SEC)
+
+
+main()
