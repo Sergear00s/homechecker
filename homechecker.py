c9e787abfe666cf17d667f335c3769b271d2b288
Algac <66782963+Sergear00s@users.noreply.github.com>
2025-01-03 12:49:24 +0000

    intra requests removed

--- homechecker.py
+++ homechecker.py
@@ -1,22 +1,62 @@
 import logging.handlers
 import requests
-import ftlib
 import config
 import time
 import logging
+import paramiko
+import re
 
 file_handler = logging.handlers.RotatingFileHandler("logs/homechecker.log", maxBytes=5000, backupCount=5)
 file_handler.setFormatter(logging.Formatter('[%(asctime)s - %(name)s - %(levelname)s - %(message)s]'))
 logger = logging.getLogger("homechecker")
 logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
+pkey = None
+with open(config.PRIVATE_KEY_PATH, "r") as fi:
+    pkey = fi.read()
 
-api = ftlib.Ftlib(config.INTRA_APP_PUBLIC, config.INTRA_APP_SECRET)
+COMMAND= "who | awk '{print $1}' | sort | uniq | grep -v 'bocal'"
 hed = {
                 "Authorization": f"Bearer {config.HOMEMAKER_TOKEN}",
                 "Accept": "application/json",
                 "Content-Type": "application/json"
         }
+
+def execute_cmd(target, username, key, command):
+    out = None
+    err = None
+    try:
+        with paramiko.SSHClient() as ssh:
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(
+                hostname=target,
+                username=username,
+                pkey=key
+            )
+            stdin, stdout, stderr = ssh.exec_command(command)
+            out = stdout.read().decode()
+            err = stderr.read().decode()
+    except:
+        pass
+    return (out, err)
+
+
+def convert_to_ip(value: str) -> str:
+    pattern = r'^k(\d+)m(\d+)s(\d+)$'
+    match = re.match(pattern, value)
+    if not match:
+        raise ValueError("format error")
+    part1 = int(match.group(1))  
+    part2 = int(match.group(2))
+    part3 = int(match.group(3)) 
+    return f"10.1{part1}.{part2}.{part3}"
+    
+
+def tgt_to_addr(tgt) -> str:
+    """iqn.fr.42:k1m40s02.42istanbul.com.tr"""
+    f = a[10::]
+    b = f[:8]
+    return convert_to_ip(b)
 
 def list_homes() -> list:
     homes = requests.get(f"{config.HOMEMAKER_ENDPOINT}/homes", headers=hed)
@@ -54,12 +94,16 @@ def check_fails():
     actives = get_active_sessions()
     for i in actives:
         user = i["identifier"]
-        user_intra = api.Users.get_user_by_login(user)
-        if (user_intra.location is None):
-            close(i["exposed_to"], user)
+        tgt = i["exposed_to"]
+        ips = tgt_to_addr(tgt)
+        out, err = execute_cmd(ips, "bocal", pkey, COMMAND)
+        if (out is None or out != user):
+            close(tgt, user)
         time.sleep(1)
 
 def main():
+    if pkey == None:
+        raise Exception("Private key is invalid!")
     while True:
         logger.debug("Checking...")
         check_fails()
